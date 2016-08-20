7a872bae49e46aeec74aaa9fb8500091dac72072
Alpri Else <alprielse@gmail.com>
2016-08-20 01:16:15 -0500

    Refactored into public directory & Added regisration form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Yung DVLP' });
-});
+/* GET home page. */  
+/*router.get('/', function(req, res, next) {
+  res.render('index', express.static('/public'));
+});*/
 
 module.exports = router;
